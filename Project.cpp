4628acfad2983237c03ce3b58759792b7bd64ab0
Ishan Kanaujiya <ishankanaujiya96@gmail.com>
2023-09-02 19:09:58 +0545

    Final Login Page Addded

--- Project.cpp
+++ Project.cpp
@@ -18,13 +18,13 @@ struct regestrationForm
 	char email[100];
 	char formUsername[100];
 	char password[100];
-}form, login;
+}form, login[100];
 int main()
 {
 
-	char a=219, roomChoice;
+	char a=219, roomChoice, accountChoice;;
 	FILE *ptr1;
-	int i=0, value=0, choice=0;
+	int i=0, value=0, j=0, choice=0, count=0, count1=0;
 	FILE *fileLogin;
 	char ch, ch1, username[100], pass[100];
 	
@@ -36,14 +36,14 @@ int main()
 	if(ch=='Y'|| ch=='y')
 	{
 		flag:
-		fileLogin=fopen("LoginPage.DAT","r");
+		fileLogin=fopen("LoginPage.DAT","rb");
 			if(fileLogin==NULL)
 			{
 				printf("\n File Not Found");
 				exit(0);
 			}
+		rewind(0);
 		fread(&login,sizeof(login),1,fileLogin);
-		
 		printf("\n                       :------------------------------------------------------:");
 		printf("\n                       :    :---------------------------------------------:   :");
 		printf("\n                       :    :                   LOGIN                     :   :");
@@ -85,19 +85,38 @@ int main()
 		}
 		system("color 07");
 		system("cls");
-		if(strcmp(username,login.formUsername)==0)
+		for(i=0; i<100; i++)
 		{
-			if(strcmp(pass,login.password)==0)
+			if(strcmp(username,login[i].formUsername)==0)
 			{
-				printf("\n Successfully Logged in into System\n\n");
-				
+				if(strcmp(pass,login[i].password)==0)
+				{
+					printf("\n Successfully Logged in into System\n\n");
+					system("pause");
+					goto flagSwitchCase;
+					count1++;
+					
+				}
 			}
-			
 		}
-		else
+		if(count1==0)
 		{
+			printf("\n ------------------------------------------------------------------------------------");
 			printf("\n  Wrong Username or Password");
-			exit(0);
+			printf("\n ------------------------------------------------------------------------------------");
+		/*	printf("\n\n\n\n");
+			printf("\n ------------------------------------------------------------------------------------");
+			printf("\n Seems Like You Don't  Have an Accout....... Do You Want To Create an Account?");
+			printf("\n If 'Yes' press 'Y' and if 'No' press 'N'");
+			printf("\n ------------------------------------------------------------------------------------\n");
+			scanf("%c", &accountChoice);
+				system("cls");
+			if(accountChoice=='Y'|| accountChoice=='y')
+			{
+			
+				goto flagRegistration;
+			}*/
+			
 		}
 		
 	//	printf("\n                  -------------------------------------------");
@@ -106,13 +125,16 @@ int main()
 	
 	else
 	{
-		fileLogin=fopen("LoginPage.DAT","w+");
+		count++;
+		flagRegistration:
+	//	system("cls");
+		fileLogin=fopen("LoginPage.DAT","ab+");
 			if(fileLogin==NULL)
 			{
 				printf("\n File Not Found");
 				exit(0);
-			}
-		
+			
+		//	flagRegistration:
 			printf("\n                           ---------------------------------------------");
 			printf("\n                                           REGESTRATION ");
 			printf("\n                           ---------------------------------------------");
@@ -136,22 +158,23 @@ int main()
 			printf("\n                    Email: ");
 			gets(form.email);
 			system("cls");
-			printf("\n            :-----------------------------------------------------:");
-			printf("\n            :    :-------------------------------------------:    :");
-			printf("\n            :    :     Set New Username and Password         :    :");
-			printf("\n            :    :-------------------------------------------:    :");
-			printf("\n            :-----------------------------------------------------:");
+			printf("\n                        :-----------------------------------------------------:");
+			printf("\n                        :    :-------------------------------------------:    :");
+			printf("\n                        :    :     Set New Username and Password         :    :");
+			printf("\n                        :    :-------------------------------------------:    :");
+			printf("\n                        :-----------------------------------------------------:");
 			printf("\n\n\n\n\n                :--------------------------------------------:");
 			printf("\n                        :      Username: ");
 			gets(form.formUsername);
-			printf("\n                    :--------------------------------------------:");
+			printf("\n                 :--------------------------------------------:");
 			fflush(stdin);
 			printf("\n                        :Password: ");
 			gets(form.password);
-			printf("\n                    :---------------------------------------------");
+			printf("\n                 :---------------------------------------------");
 			fwrite(&form,sizeof(form),1,fileLogin);
 			printf("\n Account Regestered Successfully.....\n\n");
 			system("pause");
+			//clrscr;
 			system("cls");
 			fclose(fileLogin);
 			goto flag;
@@ -180,22 +203,27 @@ int main()
 	// Hotel Information
 	flagSwitchCase:
 	printf("\n");
-	printf("\n\t\t                                 ---------------------------------------------------");
-	printf("\n\t\t                                         HOTEL RESERVATION SYSTEM ");
-	printf("\n\t\t                                 --------------------------------------------------");
+	printf("\n\t\t                                  :--------------------------------------------------:");
+	printf("\n\t\t                                  :             HOTEL RESERVATION SYSTEM             :");
+	printf("\n\t\t                                  :--------------------------------------------------:");
 	printf("\n\n\n\n\n\n\n");
-	printf("\n\t\t                                           ------------------------------");
-	printf("\n\t\t                                            |         1. Data            |");
-	printf("\n\t\t                                            ------------------------------");
-	printf("\n\t\t                                            |         2. Reservation     |");
-	printf("\n\t\t                                            ------------------------------");
-	printf("\n\t\t                                            |         3. Cancellation    |");
-	printf("\n\t\t                                            ------------------------------");
-	printf("\n\t\t                                            |         4. Information     |");
-	printf("\n\t\t                                            ------------------------------");
-	printf("\n\n\n\n\n\n                         ");
+	printf("\n\t\t                                           ---------------------------------------");
+	printf("\n\t\t                                            | S.N |          CHOICES             |");
+	printf("\n\t\t                                           ---------------------------------------");
+	printf("\n\t\t                                           ---------------------------------------");
+	printf("\n\t\t                                            |  1. |       Room Information       |");
+	printf("\n\t\t                                            --------------------------------------");
+	printf("\n\t\t                                            |  2. |       Availability           |");
+	printf("\n\t\t                                            --------------------------------------");
+	printf("\n\t\t                                            |  2. |       Reservation            |");
+	printf("\n\t\t                                            --------------------------------------");
+	printf("\n\t\t                                            |  3. |       Cancellation           |");
+	printf("\n\t\t                                            --------------------------------------");
+	printf("\n\t\t                                            |  4. |       Exit                   |");
+	printf("\n\t\t                                            --------------------------------------");
+	printf("\n\n\n\n\n                         ");
 	printf("\t\t");
-	printf("Enter Your Choice: ");
+	printf("Enter Your Choice (1-4): ");
 	scanf("%d", &choice);
 	system("cls");
 	if(choice<1 || choice>4)
@@ -257,6 +285,7 @@ int main()
 			goto flagSwitchCase;
 		break;
 					
+	}
 	}
 	return 0;
 }
