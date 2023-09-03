ab206dfcb05e2440ce4ad92a3d094c8be0777394
Ishan Kanaujiya <ishankanaujiya96@gmail.com>
2023-09-03 22:16:22 +0545

    Final Login and Registration Page added to the Program

--- Project.cpp
+++ Project.cpp
@@ -103,7 +103,8 @@ int main()
 		{
 			printf("\n ------------------------------------------------------------------------------------");
 			printf("\n  Wrong Username or Password");
-			printf("\n ------------------------------------------------------------------------------------");
+			printf("\n ------------------------------------------------------------------------------------\n");
+			exit(0);
 		/*	printf("\n\n\n\n");
 			printf("\n ------------------------------------------------------------------------------------");
 			printf("\n Seems Like You Don't  Have an Accout....... Do You Want To Create an Account?");
@@ -118,21 +119,20 @@ int main()
 			}*/
 			
 		}
-		
-	//	printf("\n                  -------------------------------------------");
 	}
 	
 	
 	else
 	{
-		count++;
-		flagRegistration:
+	//	count++;
+	//	flagRegistration:
 	//	system("cls");
 		fileLogin=fopen("LoginPage.DAT","ab+");
 			if(fileLogin==NULL)
 			{
 				printf("\n File Not Found");
 				exit(0);
+			}
 			
 		//	flagRegistration:
 			printf("\n                           ---------------------------------------------");
@@ -178,115 +178,113 @@ int main()
 			system("cls");
 			fclose(fileLogin);
 			goto flag;
-
+		}
 		
-	}
-	system("pause");
-	system("cls");
-	fclose(fileLogin);
-	
-	// Loading Code
-	
-	printf("\n\n\n\n\n\n\n\n\n");                  
-	printf("\t\t\t\t\t\t\t");
-	printf("Please Wait........\n");
-	printf("\t\t\t\t\t\t");
-	for(i=0; i<=30; i++)
-	{
-		printf("%c",a);
-		delay(50);
-	}
-	printf("\n\n\n\n\n\n\t\t");
-	system("pause");
-	system("cls");
-	
-	// Hotel Information
-	flagSwitchCase:
-	printf("\n");
-	printf("\n\t\t                                  :--------------------------------------------------:");
-	printf("\n\t\t                                  :             HOTEL RESERVATION SYSTEM             :");
-	printf("\n\t\t                                  :--------------------------------------------------:");
-	printf("\n\n\n\n\n\n\n");
-	printf("\n\t\t                                           ---------------------------------------");
-	printf("\n\t\t                                            | S.N |          CHOICES             |");
-	printf("\n\t\t                                           ---------------------------------------");
-	printf("\n\t\t                                           ---------------------------------------");
-	printf("\n\t\t                                            |  1. |       Room Information       |");
-	printf("\n\t\t                                            --------------------------------------");
-	printf("\n\t\t                                            |  2. |       Availability           |");
-	printf("\n\t\t                                            --------------------------------------");
-	printf("\n\t\t                                            |  2. |       Reservation            |");
-	printf("\n\t\t                                            --------------------------------------");
-	printf("\n\t\t                                            |  3. |       Cancellation           |");
-	printf("\n\t\t                                            --------------------------------------");
-	printf("\n\t\t                                            |  4. |       Exit                   |");
-	printf("\n\t\t                                            --------------------------------------");
-	printf("\n\n\n\n\n                         ");
-	printf("\t\t");
-	printf("Enter Your Choice (1-4): ");
-	scanf("%d", &choice);
-	system("cls");
-	if(choice<1 || choice>4)
-	{
-		printf("\n You Entered Wrong Choice...... Please Enter your Choice again!");
-		delay(100);
+		system("pause");
 		system("cls");
-		goto flag;
-	}
-	switch(choice)
-	{
-		case 1:
-			printf("\n\t\t\t\t\t\t\t First Paragraph (Introduction) ");
-			printf("\n\n\n\t\t\t\t\t\t\t Second Paragraph (Introduction) ");
-			printf("\n\n\n\n\t\t\t\t\t\t\t Third Paragraph (Introduction) ");
-			printf("\n\n\n\n\t\t\t\t\t\t\t Fourth Paragraph (Location and other details) ");
-			printf("\n\n\n\n\n\t\t\t\t ");
-			system("pause");
-			goto flag;
-		break;
+		fclose(fileLogin);
 		
-		case 2:
+		// Loading Code
+		
+		printf("\n\n\n\n\n\n\n\n\n");                  
+		printf("\t\t\t\t\t\t\t");
+		printf("Please Wait........\n");
+		printf("\t\t\t\t\t\t");
+		for(i=0; i<=30; i++)
+		{
+			printf("%c",a);
+			delay(50);
+		}
+		printf("\n\n\n\n\n\n\t\t");
+		//system("pause");
+		system("cls");
+		
+		// Hotel Information
+		flagSwitchCase:
+		printf("\n");
+		printf("\n\t\t                                  :--------------------------------------------------:");
+		printf("\n\t\t                                  :             HOTEL RESERVATION SYSTEM             :");
+		printf("\n\t\t                                  :--------------------------------------------------:");
+		printf("\n\n\n\n\n\n\n");
+		printf("\n\t\t                                           ---------------------------------------");
+		printf("\n\t\t                                            | S.N |          CHOICES             |");
+		printf("\n\t\t                                           ---------------------------------------");
+		printf("\n\t\t                                           ---------------------------------------");
+		printf("\n\t\t                                            |  1. |       Room Information       |");
+		printf("\n\t\t                                            --------------------------------------");
+		printf("\n\t\t                                            |  2. |       Availability           |");
+		printf("\n\t\t                                            --------------------------------------");
+		printf("\n\t\t                                            |  2. |       Reservation            |");
+		printf("\n\t\t                                            --------------------------------------");
+		printf("\n\t\t                                            |  3. |       Cancellation           |");
+		printf("\n\t\t                                            --------------------------------------");
+		printf("\n\t\t                                            |  4. |       Exit                   |");
+		printf("\n\t\t                                            --------------------------------------");
+		printf("\n\n\n\n\n                         ");
+		printf("\t\t");
+		printf("Enter Your Choice (1-4): ");
+		scanf("%d", &choice);
+		system("cls");
+		if(choice<1 || choice>4)
+		{
+			printf("\n You Entered Wrong Choice...... Please Enter your Choice again!");
+			delay(100);
+			system("cls");
+			goto flag;
+		}
+		switch(choice)
+		{
+			case 1:
+				printf("\n\t\t\t\t\t\t\t First Paragraph (Introduction) ");
+				printf("\n\n\n\t\t\t\t\t\t\t Second Paragraph (Introduction) ");
+				printf("\n\n\n\n\t\t\t\t\t\t\t Third Paragraph (Introduction) ");
+				printf("\n\n\n\n\t\t\t\t\t\t\t Fourth Paragraph (Location and other details) ");
+				printf("\n\n\n\n\n\t\t\t\t ");
+				system("pause");
+				goto flag;
+			break;
 			
-			ptr1 =fopen("FileFirstName.DAT","a+");
-			if(ptr1==NULL)
-			{
-				printf("\n File Not found");
-				exit(0);
-			}
-			flag1:
+			case 2:
 				
-				
-			// Scope for Filling Customers Detail	
-			
-			do
-			{
-			fflush(stdin);
-			printf("\n Enter Your Name: ");
-			gets(reservationVariable.name);
-			printf("\n Enter Your Phone Number: ");
-			scanf("%lld", &reservationVariable.phone);
-			fflush(stdin);
-			printf("\n Enter Your Address: ");
-			gets(reservationVariable.address);
-			printf("\n Enter how many room do you want to Reserve: ");
-			scanf("%d", &reservationVariable.roomNumber);
-			
-			// Writing Data to the File
-			
-			fwrite(&reservationVariable,sizeof(reservationVariable),1,ptr1);
-			printf("\n Data added Successfully");
-			
-			// Asking for more Reservarion
-			fflush(stdin);
-			printf("\n Do you want to Reserve more Room...... If Yes press Y");
-			scanf("%c", &roomChoice);
-			
-			} while(roomChoice=='Y'|| roomChoice=='y');
-			goto flagSwitchCase;
-		break;
+				ptr1 =fopen("FileFirstName.DAT","a+");
+				if(ptr1==NULL)
+				{
+					printf("\n File Not found");
+					exit(0);
+				}
+				flag1:
 					
-	}
-	}
+					
+				// Scope for Filling Customers Detail	
+				
+				do
+				{
+				fflush(stdin);
+				printf("\n Enter Your Name: ");
+				gets(reservationVariable.name);
+				printf("\n Enter Your Phone Number: ");
+				scanf("%lld", &reservationVariable.phone);
+				fflush(stdin);
+				printf("\n Enter Your Address: ");
+				gets(reservationVariable.address);
+				printf("\n Enter how many room do you want to Reserve: ");
+				scanf("%d", &reservationVariable.roomNumber);
+				
+				// Writing Data to the File
+				
+				fwrite(&reservationVariable,sizeof(reservationVariable),1,ptr1);
+				printf("\n Data added Successfully");
+				
+				// Asking for more Reservarion
+				fflush(stdin);
+				printf("\n Do you want to Reserve more Room...... If Yes press Y");
+				scanf("%c", &roomChoice);
+				
+				} while(roomChoice=='Y'|| roomChoice=='y');
+				goto flagSwitchCase;
+			break;
+						
+		}
+	
 	return 0;
 }
-
